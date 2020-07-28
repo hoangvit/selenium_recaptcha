4c6bfd316244bffa6159cb3b8f9b14c7f49e833f
Viet Hoang <burstvn@gmail.com>
2020-07-29 00:09:27 +0700

    selenum giai recaptcha

---
+++ url/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace url
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(201, 288);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Bắt Đầu";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(79, 288);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 27);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "thoat";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(79, 112);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(224, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "https://neebanker.neebank.com/auth/signup?ref=bootm4v";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(24, 44);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 31);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Số lần lặp";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.Location = new System.Drawing.Point(36, 115);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "link ref";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(79, 149);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(224, 20);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "C:\\Program Files (x86)\\HMA! Pro VPN\\Vpn.exe";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label4.Location = new System.Drawing.Point(36, 151);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "HMA";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(202, 54);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(2);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(38, 20);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(163, 56);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(27, 210);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Toa Do HMA";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(115, 205);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(44, 20);
+            this.textBox3.TabIndex = 11;
+            this.textBox3.Text = "1080";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(184, 205);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(47, 20);
+            this.textBox4.TabIndex = 12;
+            this.textBox4.Text = "600";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(78, 246);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(202, 20);
+            this.textBox5.TabIndex = 13;
+            this.textBox5.Text = "API key";
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(260, 55);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(42, 20);
+            this.numericUpDown2.TabIndex = 14;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 352);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Tool_clam_bithind";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+    }
+}
+
---
+++ url/Form1.cs
@@ -0,0 +1,407 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Keys = OpenQA.Selenium.Keys;
+using GuerrillaSharp.Models;
+using GuerrillaSharp;
+using System.Threading;
+using System.IO;
+using KAutoHelper;
+using System.Net;
+using System.Collections.Specialized;
+
+namespace url
+{
+
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void hma()
+        {
+            Process.Start(@textBox2.Text);
+            Thread.Sleep(1000);
+            AutoControl.MouseClick(int.Parse(textBox3.Text), int.Parse(textBox4.Text));
+        }
+
+        public string NgauNhienName()
+        {
+            string[] id = new string[] { "Jennifer", "Austin", "Rosie", "Crawford", "Rolando", "Harris", "Jennie", "Lambert", "Kay", "Jefferson", "Sam", "Williams", "Tricia", "Dennis", "Kelly", "Harmon", "Raquel", "Norris", "Mario", "Morris", "Melinda", "Russell", "Guillermo", "Hale", "Catherine", "Roy", "Julius", "Wagner", "Myron", "Casey", "Leona", "Lynch", "Gladys", "Mcgee", "Becky", "Parks", "Essie", "Drake", "Winston", "Wallace", "Cathy", "Benson", "Hannah", "Hoffman", "Nick", "Mann", "Leigh", "Sims", "Lisa", "Miles", "Wilfred", "Hopkins", "Elias", "Walsh", "Sarah", "Steele", "Sherman", "Green", "Wendy", "Robertson", "Jeremiah", "Barker", "Gordon", "Williamson", "Robert", "Santos", "Kim", "Washington", "Fredrick", "Webster", "Seth", "Snyder", "Kerry", "Robinson", "Javier", "Schwartz", "Elisa", "Stewart", "Marcia", "Jacobs", "Angela", "Fisher", "Rick", "Carroll", "Rosa", "Rios", "Erick", "King", "Terri", "Grant", "Penny", "Lucas", "Kayla", "Dixon", "Angelina", "Palmer", "Marta", "Bridges", "Garrett", "Mclaughlin" };
+            Random ID = new Random();
+
+            string name2 = id[ID.Next(0, id.Length)];
+            return (name2);
+        }
+
+        public string NgauNhienName2()
+        {
+            string[] id = new string[] { "Jose", "Rocco", "Sibley", "Marlyn", "Vogt", "Sharilyn", "Daily", "Ione", "Meeks", "Ines", "Taber", "Rhiannon", "Langdon", "Lina", "Fish", "Rafaela", "Oliphant", "Valene", "Doss", "Abbey", "Seaman", "Colene", "Castellanos", "Chantel", "Fuentes", "Tommie", "Raymond", "Reynolds", "Roosevelt", "Harvey", "Ronnie", "Martinez", "Erik", "Carson", "Leslie", "Arnold", "Doreen", "Chavez", "Jaime", "Garcia", "Celia", "Alexander", "Winston", "Peters", "Richard", "Parks", "Veronica", "Nunez", "Kristy", "Ray", "Terry", "Thompson", "Frank", "Patrick", "Marcia", "Richards", "Aubrey", "Olson", "Joanna", "Johnston", "Darnell", "Santos", "Rochelle", "Harmon", "Nicholas", "Cross", "Steve", "Lee", "Beatrice", "Vega", "Edith", "Wagner", "Amelia", "Miller", "Essie", "Powell", "Martin", "Chandler", "Nick", "Malone", "Tamara", "Rodriquez", "Everett", "Phillips", "Abraham", "Hayes", "Lamar", "Brewer", "Pablo", "Luna", "Jaime", "Adams", "Carmen", "Pierce", "Kay", "Zimmerman", "Bobbie", "Glover", "Casey", "Little" };
+
+            Random ID = new Random();
+            string name2 = id[ID.Next(0, id.Length)];
+            return (name2);
+
+        }
+
+        string Reslove2CaptchaCaptcha(string captchaKey, string ggKey, string url)
+        {
+            string capt = "";
+
+            Recaptcha_2Captcha reCapt = new Recaptcha_2Captcha(captchaKey);
+
+            bool isSuccess = reCapt.SolveRecaptchaV2(ggKey, url, out capt);
+
+            while (!isSuccess)
+            {
+                isSuccess = reCapt.SolveRecaptchaV2(ggKey, url, out capt);
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+            }
+
+            return capt;
+        }
+
+
+        public void chrome(int ii)
+        {
+            GuerrillaMail guerrilla = new GuerrillaMail();
+            guerrilla.GetEmailAddress();
+
+
+
+            Random number = new Random();
+            string sdt = "09" + number.Next(10000000, 99999999);
+            string passrandom = NgauNhienName().ToLower() + number.Next(10000, 99999) + "@";
+            string mail = guerrilla.EmailAddress;
+            string ggkey = "6LdKwvcUAAAAAOWHayxQNHnDrDVL6GWC-lx1EP0R";
+            string CaptchaURL = "https://neebanker.neebank.com/auth/signup?ref=bootm4v";
+            var usename = NgauNhienName() + number.Next(0, 999999) + ii;
+            var email = guerrilla.EmailAddress;
+
+            
+
+
+
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+            service.HideCommandPromptWindow = true;
+
+            //var options = new ChromeOptions();
+            //options.AddArgument("--start - maximized");
+
+            var chromeDriver = new ChromeDriver(service);
+
+            chromeDriver.Url = textBox1.Text;
+            chromeDriver.Navigate();
+            try
+            {
+                Thread.Sleep(10000);
+                chromeDriver.FindElementByCssSelector("#id_username").SendKeys(usename);
+            }
+            catch
+            {
+                chromeDriver.Navigate().Refresh();
+                chromeDriver.FindElementByCssSelector("#id_username").SendKeys(usename); ;
+            }
+            string captchaOut = Reslove2CaptchaCaptcha(textBox5.Text, ggkey, CaptchaURL);
+            chromeDriver.FindElementByCssSelector("#CountryCode > option:nth-child(233)").Click();
+            chromeDriver.FindElementByCssSelector("#userPhone").SendKeys(sdt);
+            chromeDriver.FindElementByCssSelector("#id_email").SendKeys(email);
+            chromeDriver.FindElementByCssSelector("#id_password1").SendKeys(passrandom);
+            chromeDriver.FindElementByCssSelector("#id_password2").SendKeys(passrandom);
+            chromeDriver.FindElementByCssSelector("#signupform > div.form-group > div.custom-control.custom-checkbox.d-flex.justify-content-start.text-white > label").Click();
+            
+            chromeDriver.ExecuteScript("document.getElementById(\"g-recaptcha-response\").innerHTML=\""+captchaOut+"\"");
+            chromeDriver.FindElementByCssSelector("#signup-btn > span").Click();
+            Thread.Sleep(15000);
+            chromeDriver.ExecuteScript("document.getElementsByClassName('confirm btn btn-lg btn-success')[0].click();");
+
+            //chromeDriver.FindElementByCssSelector("body > div.sweet-alert.showSweetAlert.visible > div.sa-button-container > div > button").Click();
+      
+
+
+
+
+
+
+
+            guerrilla.CheckEmail();
+
+
+
+            if (guerrilla.EmailCount == 0)
+            {
+                Thread.Sleep(15000);
+                guerrilla.CheckEmail();
+                if (guerrilla.EmailCount == 0)
+                {
+                    guerrilla.CheckEmail();
+                    Thread.Sleep(15000);
+                    guerrilla.CheckEmail();
+                }
+            }
+            if (guerrilla.EmailCount > 0)
+            {
+
+                Email emails = guerrilla.FetchEmail(guerrilla.Emails[0].MailId);
+
+                string url = emails.MailBody;
+                var aaaa2 = url.Substring(url.IndexOf("bold"), 13);
+                Thread.Sleep(3000);
+                var code = aaaa2.Split('>')[1];
+                chromeDriver.FindElementByCssSelector("#TwoFaCode").SendKeys(code);
+                chromeDriver.FindElementByCssSelector("#otp-activation-btn").Click();
+                Thread.Sleep(3000);
+                
+
+            }
+            
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Thread> threads = new List<Thread>();
+            for (var j = 0; j < numericUpDown2.Value; j++)
+            {
+                for (var i = 0; i < numericUpDown1.Value; i++)
+                {
+                    label1.Text = (i + 1).ToString();
+                    var val = i;
+                    Thread t1 = new Thread(() => { chrome(val); });
+                    t1.Start();
+                    t1.Join(3000);
+                    threads.Add(t1);
+                    Thread.Sleep(2000);
+
+                }
+                for (var i = 0; i < threads.Count; i++)
+                {
+                    if (threads[i].IsAlive == false)
+                    {
+                        threads.Remove(threads[i]);
+                    }
+
+                }
+
+            }
+        }
+
+
+        private void xuatDuLieu(string use)
+        {
+
+            try
+            {
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter("v8.txt", true))
+                {
+                    file.WriteLine(use);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("lost :", ex);
+            }
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    public class Recaptcha_2Captcha
+    {
+        public string APIKey { get; private set; }
+        public Recaptcha_2Captcha(string apiKey)
+        {
+            APIKey = apiKey;
+        }
+
+        /// <summary>
+        /// Sends a solve request and waits for a response
+        /// </summary>
+        /// <param name="googleKey">The "sitekey" value from site your captcha is located on</param>
+        /// <param name="pageUrl">The page the captcha is located on</param>
+        /// <param name="proxy">The proxy used, format: "username:password@ip:port</param>
+        /// <param name="proxyType">The type of proxy used</param>
+        /// <param name="result">If solving was successful this contains the answer</param>
+        /// <returns>Returns true if solving was successful, otherwise false</returns>
+        public bool SolveRecaptchaV2(string googleKey, string pageUrl, out string result)
+        {
+            string requestUrl = "http://2captcha.com/in.php?key=" + APIKey + "&method=userrecaptcha&googlekey=" + googleKey + "&pageurl=" + pageUrl;
+            try
+            {
+                WebRequest req = WebRequest.Create(requestUrl);
+
+                using (WebResponse resp = req.GetResponse())
+                using (StreamReader read = new StreamReader(resp.GetResponseStream()))
+                {
+                    string response = read.ReadToEnd();
+
+                    if (response.Length < 3)
+                    {
+                        result = response;
+                        return false;
+                    }
+                    else
+                    {
+                        if (response.Substring(0, 3) == "OK|")
+                        {
+                            string captchaID = response.Remove(0, 3);
+
+                            for (int i = 0; i < 24; i++)
+                            {
+                                WebRequest getAnswer = WebRequest.Create("http://2captcha.com/res.php?key=" + APIKey + "&action=get&id=" + captchaID);
+
+                                using (WebResponse answerResp = getAnswer.GetResponse())
+                                using (StreamReader answerStream = new StreamReader(answerResp.GetResponseStream()))
+                                {
+                                    string answerResponse = answerStream.ReadToEnd();
+
+                                    if (answerResponse.Length < 3)
+                                    {
+                                        result = answerResponse;
+                                        return false;
+                                    }
+                                    else
+                                    {
+                                        if (answerResponse.Substring(0, 3) == "OK|")
+                                        {
+                                            result = answerResponse.Remove(0, 3);
+                                            return true;
+                                        }
+                                        else if (answerResponse != "CAPCHA_NOT_READY")
+                                        {
+                                            result = answerResponse;
+                                            return false;
+                                        }
+                                    }
+                                }
+
+                                Thread.Sleep(5000);
+                            }
+
+                            result = "Timeout";
+                            return false;
+                        }
+                        else
+                        {
+                            result = response;
+                            return false;
+                        }
+                    }
+                }
+            }
+            catch { }
+
+            result = "Unknown error";
+            return false;
+        }
+
+        /// <summary>
+        /// Slove normal capcha wroted by K9 from Kteam
+        /// You have to get the capcha image from the website then convert to base64
+        /// </summary>
+        /// <param name="base64Image"></param>
+        /// <param name="result"></param>
+        /// <returns></returns>
+        public bool SolveNormalCapcha(string base64Image, out string result)
+        {
+            string response = "";
+            using (var client = new WebClient())
+            {
+                var values = new NameValueCollection();
+                values["method"] = "base64";
+                values["key"] = APIKey;
+                values["body"] = base64Image;
+                var res = client.UploadValues("http://2captcha.com/in.php", values);
+                response = Encoding.Default.GetString(res);
+            }
+
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+            if (response.Substring(0, 3) == "OK|")
+            {
+                string captchaID = response.Remove(0, 3);
+
+                for (int i = 0; i < 24; i++)
+                {
+                    WebRequest getAnswer = WebRequest.Create("http://2captcha.com/res.php?key=" + APIKey + "&action=get&id=" + captchaID);
+
+                    using (WebResponse answerResp = getAnswer.GetResponse())
+                    using (StreamReader answerStream = new StreamReader(answerResp.GetResponseStream()))
+                    {
+                        string answerResponse = answerStream.ReadToEnd();
+
+                        if (answerResponse.Length < 3)
+                        {
+                            result = answerResponse;
+                            return false;
+                        }
+                        else
+                        {
+                            if (answerResponse.Substring(0, 3) == "OK|")
+                            {
+                                result = answerResponse.Remove(0, 3);
+                                return true;
+                            }
+                            else if (answerResponse != "CAPCHA_NOT_READY")
+                            {
+                                result = answerResponse;
+                                return false;
+                            }
+                        }
+                    }
+
+                    Thread.Sleep(5000);
+                }
+
+                result = "Timeout";
+                return false;
+            }
+            else
+            {
+                result = response;
+                return false;
+            }
+        }
+    }
+}
+
